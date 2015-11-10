70762c7ba10c919d914ae37a1cf358670fea1471
kevin <kevinzheng1983@gmail.com>
2015-11-10 21:40:14 +1000

    Add segmentation interface and a few implementations

---
+++ core/src/main/java/com/traminer/preprocess/compression/TrajectoryCompression.java
@@ -0,0 +1,7 @@
+package com.traminer.preprocess.compression;
+
+/**
+ * Created by kevin on 10/11/15.
+ */
+public interface TrajectoryCompression {
+}
---
+++ core/src/main/java/com/traminer/preprocess/segmentation/SegmentationTest.java
@@ -0,0 +1,32 @@
+package com.traminer.preprocess.segmentation;
+
+import com.traminer.base.Trajectory;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Created by kevin on 10/11/15.
+ */
+public class SegmentationTest {
+
+    public static void main(String[] args) {
+
+        DailySegmentation dailySegmentation = new DailySegmentation();
+        MaxGapSegmentation maxGapSegmentation = new MaxGapSegmentation();
+        MaxStaySegmentation maxStaySegmentation = new MaxStaySegmentation();
+
+        Trajectory trajectory = new Trajectory();
+        List<Trajectory> results = Stream.of(trajectory)
+                .flatMap(t -> dailySegmentation.doSegmentation(t).stream())
+                .flatMap(t -> maxGapSegmentation.doSegmentation(t).stream())
+                .flatMap(t -> maxStaySegmentation.doSegmentation(t).stream())
+                .filter(t -> t.size() > 5)
+                .collect(Collectors.toList());
+
+        results.forEach(System.out::println);
+
+    }
+}

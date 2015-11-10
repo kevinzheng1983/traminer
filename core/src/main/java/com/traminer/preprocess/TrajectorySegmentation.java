bd4638f4d6f975950107d3a271ce31764ec7de53
Kevin Zheng <kevinzheng1983@gmail.com>
2015-11-10 14:36:23 +1000

    add TrajectorySegmentation interface

---
+++ core/src/main/java/com/traminer/preprocess/TrajectorySegmentation.java
@@ -0,0 +1,13 @@
+package com.traminer.preprocess;
+
+import com.traminer.base.Trajectory;
+
+import java.util.List;
+
+/**
+ * A common interface for segmenting a long trajectory into multiple short, more meaningful trajectories.
+ * @author Kevin Zheng
+ */
+public interface TrajectorySegmentation {
+    List<Trajectory> doSegmentation(Trajectory trajectory);
+}

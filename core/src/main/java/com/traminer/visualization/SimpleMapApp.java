02a6b5993e7f4fa22a0eef17df584ea97771f165
Kevin Zheng <kevinzheng1983@gmail.com>
2015-11-10 11:31:59 +1000

    Modify SimpleMapApp

--- core/src/main/java/com/traminer/visualization/SimpleMapApp.java
+++ core/src/main/java/com/traminer/visualization/SimpleMapApp.java
@@ -5,8 +5,10 @@ import de.fhpotsdam.unfolding.data.Feature;
 import de.fhpotsdam.unfolding.data.GeoJSONReader;
 import de.fhpotsdam.unfolding.geo.Location;
 import de.fhpotsdam.unfolding.marker.Marker;
+import de.fhpotsdam.unfolding.marker.SimplePointMarker;
 import de.fhpotsdam.unfolding.providers.Google;
 import de.fhpotsdam.unfolding.utils.MapUtils;
+import de.fhpotsdam.unfolding.utils.ScreenPosition;
 import processing.core.PApplet;
 
 import java.util.List;
@@ -20,6 +22,8 @@ public class SimpleMapApp extends PApplet {
 
     Location beijing = new Location(39.9167, 116.3833);
 
+    SimplePointMarker beijingMaker;
+
     public void setup() {
         size(1000, 700);
 
@@ -29,11 +33,24 @@ public class SimpleMapApp extends PApplet {
         // Add mouse and keyboard interactions
         MapUtils.createDefaultEventDispatcher(this, map);
 
+        beijingMaker = new SimplePointMarker(beijing);
+
     }
 
 
     public void draw() {
         map.draw();
+
+        ScreenPosition posBeijing = beijingMaker.getScreenPosition(map);
+        strokeWeight(12);
+        stroke(200, 0, 0, 200);
+        strokeCap(SQUARE);
+        noFill();
+        float s = 44;
+        arc(posBeijing.x, posBeijing.y, s, s, -PI * 0.9f, -PI * 0.1f);
+        arc(posBeijing.x, posBeijing.y, s, s, PI * 0.1f, PI * 0.9f);
+        fill(0);
+        text("BEIJING", posBeijing.x - textWidth("BEIJING") / 2, posBeijing.y + 4);
     }
 
 

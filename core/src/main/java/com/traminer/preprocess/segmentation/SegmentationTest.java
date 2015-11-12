e418b89300a8b48c1af72a0302e28995ec53ed75
bob <oldtong2@hotmail.com>
2015-11-12 16:06:24 +1000

    start to segmentation function

--- core/src/main/java/com/traminer/preprocess/segmentation/SegmentationTest.java
+++ core/src/main/java/com/traminer/preprocess/segmentation/SegmentationTest.java
@@ -1,6 +1,8 @@
 package com.traminer.preprocess.segmentation;
 
 import com.traminer.base.Trajectory;
+import com.traminer.reader.SimpleTrajectoryReader;
+import com.traminer.reader.TrajectoryReader;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -13,19 +15,25 @@ public class SegmentationTest {
 
     public static void main(String[] args) {
 
-        DailySegmentation dailySegmentation = new DailySegmentation();
+        //DailySegmentation dailySegmentation = new DailySegmentation();
         MaxGapSegmentation maxGapSegmentation = new MaxGapSegmentation();
         MaxStaySegmentation maxStaySegmentation = new MaxStaySegmentation();
 
-        Trajectory trajectory = new Trajectory();
+        TrajectoryReader tr = new SimpleTrajectoryReader();
+
+        Trajectory trajectory = tr.readTrajectory("/Users/haozhouwang/MyUQ/expdata/testData/281");
         List<Trajectory> results = Stream.of(trajectory)
-                .flatMap(t -> dailySegmentation.doSegmentation(t).stream())
                 .flatMap(t -> maxGapSegmentation.doSegmentation(t).stream())
                 .flatMap(t -> maxStaySegmentation.doSegmentation(t).stream())
                 .filter(t -> t.size() > 5)
                 .collect(Collectors.toList());
 
-        results.forEach(System.out::println);
+        for(Trajectory t : results){
+           // System.out.println(t.toString());
+           // System.out.println("-------------------------");
+        }
+
+        //results.forEach(System.out::println);
 
     }
 }
